7e358762c4d1358e4ee48d17e784e4e7d9f8768a
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-03-30 14:19:16 +0900

    Selecting text enabled.

--- jrpan.js
+++ jrpan.js
@@ -1,29 +1,45 @@
-var tokenizer;
+const kuromojiLoaded = () =>
+	new Promise((resolve, reject) => {
+		kuromoji.builder({ dicPath: './dict' }).build((error, _tokenizer) => {
+			if (error != null) console.log(error)
+			resolve(_tokenizer)
+		})
+	})
 
-kuromoji.builder({ dicPath: '/dict/' }).build((error, _tokenizer) => {
-	if (error != null) console.log(error);
-	tokenizer = _tokenizer;
-	console.log('loaded');
-});
+let selectedText
+let wholeText
+getSelectionText = () => {
+	var text = ''
+	if (window.getSelection) {
+		text = window.getSelection().toString()
+	} else if (document.selection && document.selection.type != 'Control') {
+		text = document.selection.createRange().text
+	}
+	return text
+}
 
-$(document).ready(() => {
-	var content;
+kuromojiLoaded().then(res => {
+	document.addEventListener('mouseup', e => {
+		selectedText = getSelectionText()
 
-	var getContent = setInterval(() => {
-		if ($('.ii.gt')) {
-			content = $('.ii.gt')
-				.text()
-				.trim();
-			if (content.length > 0) {
-				showContent();
-				clearInterval(getContent);
+		Object.values(document.getElementsByClassName('jrpan-selection')).map(
+			(e, i) => {
+				e.parentNode.innerHTML = e.parentNode.innerText
 			}
+		)
+		if (selectedText !== '') {
+			wholeText = e.target.innerText
+			var re = new RegExp(selectedText, 'g')
+			e.target.innerHTML = wholeText.replace(
+				re,
+				`<b class="jrpan-selection" style="color: red; position: relative">${selectedText}</b>`
+			)
+		} else {
+			Object.values(document.getElementsByClassName('jrpan-selection')).map(
+				(e, i) => {
+					e.innerHTML = e.innerText
+				}
+			)
 		}
-	}, 1000);
-
-	var showContent = function() {
-		$('.ii.gt').text('Hello!');
-	};
-});
-
-// if (document.querySelector('.ii') !== null) console.log('test');
+	})
+})
