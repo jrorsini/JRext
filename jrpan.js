649b258b82882d89b5a6247123ebe02e36dd6dc8
Jean-Roger <jeanroger.orsini@gmail.com>
2018-04-01 01:50:39 +0900

    Minor.

--- jrpan.js
+++ jrpan.js
@@ -26,9 +26,9 @@ const getTranslation = word =>
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState === 4) {
 				let data = JSON.parse(xhr.responseText)['data'][0]
-				console.log(data['japanese'][0]['word'])
-				console.log(data['japanese'][0]['reading'])
-				console.log(data['senses'][0]['english_definitions'])
+				// console.log(data['japanese'][0]['word'])
+				// console.log(data['japanese'][0]['reading'])
+				// console.log(data['senses'][0]['english_definitions'])
 				resolve(data)
 			} else if (xhr.status !== 200) {
 				console.log(xhr.responseText)
@@ -41,28 +41,39 @@ const createElement = () => {
 	let node = document.createElement('div')
 	node.id = 'jrpan-block'
 	document.body.appendChild(node)
-	document.getElementById('jrpan-block').innerHTML =
-		'<button class="jrpan-btn">JRpan it</button>'
+}
+
+const fillPopup = data => {
+	document.getElementById('jrpan-block').innerHTML = ''
+	document.getElementById('jrpan-block').innerHTML = `
+	<div class="jrpan-popup">
+		<small>${data['japanese'][0]['word']}</small>
+		<p><b>${data['japanese'][0]['reading']}</b></p>
+		<i>${data['senses'][0]['english_definitions'].join(', ')}</i>
+	</div>
+	<button class="jrpan-btn">JRpan it</button>
+`
 }
 
 createElement()
 
 document.addEventListener('mouseup', e => {
 	selectedText = getSelectionText()
-
-	Object.values(document.getElementsByClassName('jrpan-selection')).map(
-		(e, i) => {
-			e.parentNode.innerHTML = e.parentNode.innerText
-		}
-	)
+	if (document.getElementsByClassName('jrpan-selection').length) {
+		Object.values(document.getElementsByClassName('jrpan-selection')).map(
+			(e, i) => {
+				e.parentNode.innerHTML = e.parentNode.innerText
+			}
+		)
+	}
 	if (selectedText !== '') {
 		wholeText = e.target.innerText
-		var re = new RegExp(selectedText, 'g')
-		e.target.innerHTML = wholeText.replace(
-			re,
-			`<b class="jrpan-selection" style="${txtStyle}">${selectedText}</b>`
-		)
-		getTranslation(selectedText).then(res => {})
+		const re = new RegExp(selectedText, 'g')
+		const selectionElement = `<b class="jrpan-selection" style="${txtStyle}">${selectedText}</b>`
+		e.target.innerHTML = wholeText.replace(re, selectionElement)
+		getTranslation(selectedText).then(res => {
+			fillPopup(res)
+		})
 	} else {
 		Object.values(document.getElementsByClassName('jrpan-selection')).map(
 			(e, i) => {
