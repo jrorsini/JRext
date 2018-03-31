ecc659bcb114a040ad183a3b8237c17729ea89bd
Jean-Roger <jeanroger.orsini@gmail.com>
2018-03-31 18:41:34 +0900

    Minro

--- jrpan.js
+++ jrpan.js
@@ -1,5 +1,5 @@
 let txtStyle = `
-	background: -webkit-linear-gradient(#f30065, #ff7e8a);;
+	background: -webkit-linear-gradient(#f30065, #ff7e8a);
 	-webkit-background-clip: text;
 	-webkit-text-fill-color: transparent; 
 	position: relative;
@@ -7,7 +7,7 @@ let txtStyle = `
 let selectedText
 let wholeText
 
-getSelectionText = () => {
+let getSelectionText = () => {
 	var text = ''
 	if (window.getSelection) {
 		text = window.getSelection().toString()
@@ -16,6 +16,33 @@ getSelectionText = () => {
 	}
 	return text
 }
+
+const getTranslation = word =>
+	new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest()
+		xhr.open('GET', `https://jisho.org/api/v1/search/words?keyword=${word}/`)
+		xhr.setRequestHeader('Accept', 'application/json')
+		xhr.send()
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4) {
+				console.log(xhr.responseText)
+				resolve(xhr.responseText)
+			} else if (xhr.status !== 200) {
+				console.log(xhr.responseText)
+				resolve(xhr.responseText)
+			}
+		}
+	})
+
+const createElement = () => {
+	let node = document.createElement('div')
+	node.id = 'jrpan-block'
+	document.body.appendChild(node)
+	document.getElementById('jrpan-block').innerHTML =
+		'<button class="jrpan-btn">JRpan it</button>'
+}
+
+createElement()
 
 document.addEventListener('mouseup', e => {
 	selectedText = getSelectionText()
@@ -32,6 +59,7 @@ document.addEventListener('mouseup', e => {
 			re,
 			`<b class="jrpan-selection" style="${txtStyle}">${selectedText}</b>`
 		)
+		getTranslation(selectedText).then(res => {})
 	} else {
 		Object.values(document.getElementsByClassName('jrpan-selection')).map(
 			(e, i) => {
