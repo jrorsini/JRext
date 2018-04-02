1c837ded892070aed840bbf9144e1f887d7960ce
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-04-02 14:40:59 +0900

    Minor.

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 'use strict'
 
-function click(e) {
-	chrome.tabs.executeScript(null, {
-		file: 'jrpan.js'
-	})
-	window.close()
+let injected = false
+const click = e => {
+	if (!injected) {
+		chrome.tabs.executeScript(null, {
+			file: 'jrpan.js'
+		})
+		window.close()
+	}
 }
 
 document.addEventListener('DOMContentLoaded', function() {
