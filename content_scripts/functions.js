60e1e6ff82564b3509290bec7ab9f6836bd0fdce
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-04-05 15:08:36 +0900

    Minor.

--- content_scripts/functions.js
+++ content_scripts/functions.js
@@ -3,6 +3,20 @@
  * @return {String} marked up from kuromoji.
  */
 const kuromojiMarkup = selection => {
+	const part_of_speech = {
+		名詞: 'noun',
+		動詞: 'verb',
+		記号: 'symbol',
+		副詞: 'adverb',
+		助詞: 'particle',
+		助動詞: 'auxiliaryVerb',
+		接頭詞: 'prefix',
+		接続詞: 'conjuction',
+		形容詞: 'i-adjective',
+		連体詞: 'abdominalAdj',
+		感動詞: 'interjection',
+		フィラー: 'filler'
+	}
 	let path = tokenizer.tokenizeForSentence(selection)
 	path.map(e => {
 		if (part_of_speech[e.pos] === undefined) {
