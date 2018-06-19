6dd351b325a80b0e5362d3e413137d41d496c6fa
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-06-19 11:52:00 +0900

    Getting back over JRpan.

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 
 const click = e => {
 	chrome.tabs.insertCSS(null, {
 		file: 'jrpan.css'
-	})
-	window.close()
-}
+	});
+	window.close();
+};
 document.addEventListener('DOMContentLoaded', function() {
 	// document.getElementById('activationBtn').addEventListener('click', click)
-})
+});
